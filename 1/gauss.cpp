3e07689e94a0849d3649e0629d35d2b29b557467
DariaSharova228 <dariasharova02@gmail.com>
2022-12-09 20:30:58 +0300

    gauss

--- 1/gauss.cpp
+++ 1/gauss.cpp
@@ -1,174 +1,139 @@
-#include <stdio.h>
-#include <time.h>
 #include "matrix.h"
-int main(int argc, char *argv[]){
-    double* A;
-    double* B;
-    double* X;
-    double* block1;
-    double* block2;
-    double* block3;
-    double* inv_block;
-    int res = 0;
-    int n = 0, m = 0, r = 0, s = 0;
-    char* name_file;
-    int task = 9;
-    double r1 = 0, r2 = 0, t1 = 0, t2 = 0;
-    FILE* inp;
-    if(!((argc != 5) || (argc != 6))){
+
+int main(int argc, char *argv[]) {
+    Args *args = nullptr;
+    Results *res = nullptr;
+    Global_results g_r;
+    int n = 0, m = 0, r = 0, s = 0, p = 0, u = 0, l = 0, k = 0;
+    double *A = nullptr;
+    double *X = nullptr;
+    double *B = nullptr;
+    char *name = nullptr;
+    //double r1 = -1, r2 = -1, t1 = 0, t2 = 0;
+    //FILE *inp;
+
+    if (!((argc == 6 || argc == 7) 
+    && sscanf(argv[1], "%d", &n)
+    && sscanf(argv[2], "%d", &m)
+    && sscanf(argv[3], "%d", &p)
+    && sscanf(argv[4], "%d", &r)
+    && sscanf(argv[5], "%d", &s)
+    && (s >= 0) && (s <= 4)) || (argc == 6 && s == 0)) {
+        printf("%s Usage: n m p r s [file]\n", argv[0]);
         return 0;
     }
-    if(!(sscanf(argv[1], "%d", &n) == 1 && 
-    sscanf(argv[2], "%d", &m) == 1 && 
-    sscanf(argv[3], "%d", &r) == 1 && 
-    ((sscanf(argv[4], "%d", &s) == 1 &&
-    (s >= 0 && s <= 4)) || (s == 0 && argc == 5))
-    )) {
-        return 0;
-    }
-    A = new double[n*n];
-    if(!A){
-        return 0;
-    }
-    B = new double[n];
-    if(!B)
-    {
-        delete []A;
+    
+    A = new double[n * n];
+    if (!A) {
+        printf("MEMORY ERROR!!!\n");
         return 0;
     }
     X = new double[n];
-    if(!X){
+    if (!X) {
+        printf("MEMORY ERROR!!!\n");
         delete []A;
-        delete []B;
         return 0;
     }
-    if(s == 0) {
-        name_file = argv[5];
-        inp = fopen(name_file, "r");
-        if(!inp){
-            delete []A;
-            delete []B;
-            delete []X;
-            return 0;
-        }
-        if(read_matrix(inp, A, n) == 0 ) {
-            printf("ошибка в файле с матрицей\n");\
-            fclose(inp);
-            delete []A;
-            delete []B;
-            delete []X;
-            return 0;
-        }
-        initialization_B(B, A, n);
-        fclose(inp);
-    }
-    else {
-        fill_matrix(A, n, s);
-        initialization_B(B, A, n);
-    }
-    printf("A =\n");
-  print_matrix(A, n, n, r);
-  printf("\n");
-  printf("B =\n");
-  print_matrix(B, n, 1, r);
-  printf("\n");
-  //int k = n/m;
-  //int l = n - m * k;
-  block1 = new double[m*m];
-  block2 = new double[m*m];
-  block3 = new double[m*m];
-  inv_block = new double [m*m];
-  for(int i = 0; i < n; i++) {
-    X[i] = 0;
-  }
-  /*double normmatrix = norm_block(A, n);
-  get_block(0, 0, n, m, k, l, A, block1);
-  print_block(block1, m, m);
-  printf("\n");
-  get_block_b(0, m, k, l , B, block2);
-  print_block(block2, m, 1);
-  printf("\n");
-  mult_blocks(block1, block2, block3, m, m, 1);
-  print_block(block3, m, 1);
-  printf("\n");*/
-  /*get_block(0, 0, n, m, k, l, A, block1);
-  print_block(block1, m, m);
-  printf("\n");
-  get_block(1, 0, n, m, k, l, A, block2);
-  print_block(block2, l, m);
-  printf("\n");
-  mult_blocks(block2, block1, block3, m, l, m);
-  print_block(block3, l, m);
-  printf("\n");*/
-  /*printf("B =\n");
-  print_matrix(B, n, 1, r);*/
-    t1 = clock();
-    res = solution(A, B, X, n, m, block1, block2, inv_block, block3);
-    //printf("A =\n");
-  //print_matrix(A, n, n, r);
-  //printf("\n");
-  //printf("B =\n");
-  //print_matrix(B, n, 1, r);
-  //printf("\n");
-    t1 = (clock() - t1)/ CLOCKS_PER_SEC;
-    if (res != 1){
-        r1 = -1;
-        r2 = -1;
-        printf ("%s : Task = %d Res1 = %e Res2 = %e T1 = %.2f T2 = %.2f S = %d N = %d M = %d\n", argv[0], task, r1, r2, t1, t2, s, n, m);
+
+    B = new double[n];
+    if (!B) {
+        printf("MEMORY ERROR!!!\n");
         delete []A;
-        delete []B;
         delete []X;
-        delete [] block1;
-        delete [] block2;
-        delete [] block3;
-        delete [] inv_block;
         return 0;
     }
-    printf("X =\n");
-    print_matrix(X, n, 1, r);
-    if(s == 0) {
-        name_file = argv[5];
-        inp = fopen(name_file, "r");
-        if(!inp){
-            delete []A;
-            delete []B;
-            delete []X;
-            delete [] block1;
-            delete [] block2;
-            delete [] block3;
-            delete [] inv_block;
-            return 0;
-        }
-        if(read_matrix(inp, A, n) == 0 ) {
-            printf("ошибка в файле с матрицей\n");\
-            fclose(inp);
-            delete []A;
-            delete []B;
-            delete []X;
-            delete [] block1;
-            delete [] block2;
-            delete [] block3;
-            delete [] inv_block;
-            return 0;
-        }
-        initialization_B(B, A, n);
-        fclose(inp);
+
+    if (s == 0) {
+        name = argv[6];
     }
-    else {
-        fill_matrix(A, n, s);
-        initialization_B(B, A, n);
+
+    res = new Results[p];
+    if (res == nullptr) {
+        printf("MEMORY ERROR!!!\n");
+        delete []A;
+        delete []X;
+        delete []B;
+        return 0;
     }
-    t2 = clock();
-    r1 = discrepancy1(A, B, X, n);
-    r2 = discrepancy2(X, n);
-    t2 = (clock() - t2)/CLOCKS_PER_SEC;
-    printf ("%s : Task = %d Res1 = %e Res2 = %e T1 = %.2f T2 = %.2f S = %d N = %d M = %d\n", argv[0], task, r1, r2, t1, t2, s, n, m);
+
+    args = new Args[p];
+    if (args == nullptr) {
+        printf("MEMORY ERROR!!!\n");
+        delete []A;
+        delete []X;
+        delete []B;
+        delete []res;
+        return 0;
+    }
+
+    k = n / m;
+    l = n % m;
+    for(u = 0; u < p; u++) {
+        args[u].A = A;
+        args[u].B = B;
+        args[u].X = X;
+        args[u].name = name;
+        args[u].argv_0 = argv[0];
+        args[u].n = n;
+        args[u].m = m;
+        args[u].r = r;
+        args[u].s = s;
+        args[u].u = u;
+        args[u].p = p;
+        args[u].k = k;
+        args[u].l = l;
+        args[u].res = res;
+        args[u].global_res = &g_r;
+    }
+
+    for(u = 1; u < p; u++) {
+        if(pthread_create(&args[u].t_id, 0, thread_func, args + u)) {
+            printf("COULD NOT CREATE PTHREAD %d!\n", u);
+            delete []A;
+            delete []X;
+            delete []B;
+            delete []res;
+            delete []args;
+            abort();
+        }
+    }
+
+    thread_func(args + 0);
+    for(u = 1; u < p; u++) {
+        pthread_join(args[u].t_id, 0);
+    }
+
+    /*printf("_____________________________________________________\n A\n");
+    printMatrix(r, n, n, a);*/
+
+    /*if (res == 0) {
+        printf("_____________________________________________________\n");
+        printf("               METHOD CANNOT BE USED!!!\n");
+        printf("_____________________________________________________\n");
+    }*/
+    /*if (g_r.err < 0)
+        switch (g_r.err) {
+        case -1:
+            printf ("%s : Task = %d Res1 = %e Res2 = %e T1 = %.2f T2 = %.2f S = %d N = %d M = %d P = %d\n", argv[0], task, -1., -1., 0., 0., s, n, m, p);
+            break;
+        case -2:
+            printf("COULD NOT OPEN FILE %s!\n", name);
+            break;
+        case -3:
+            printf("COULD NOT READ FILE %s!\n", name);
+            break;
+        case -4:
+            printf("NOT ENOUGH MEMORY %s!\n", name);
+            break;
+        default:
+            break;
+        }*/
+    if (g_r.err < 0) printf ("%s : Task = %d Res1 = %e Res2 = %e T1 = %.2f T2 = %.2f S = %d N = %d M = %d P = %d\n", argv[0], 9, -1., -1., 0., 0., s, n, m, p);
+
     delete []A;
-    delete []B;
     delete []X;
-    delete []block1;
-    delete []block2;
-    delete []block3;
-    delete []inv_block;
+    delete []B;
+    delete []args;
+    delete []res;
     return 0;
 }
